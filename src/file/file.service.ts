72e733437fdc6ecd7548279aa80b186d9faa3f5f
Jan Jakubik <janekjak@gmail.com>
2021-02-22 21:54:26 +0100

    fix package.json 10

--- src/file/file.service.ts
+++ src/file/file.service.ts
@@ -1,8 +1,9 @@
-import {Injectable} from '@nestjs/common';
+import {Injectable, Logger} from '@nestjs/common';
 import {existsSync, promises} from "fs";
 import {resolve} from "path";
 import {REGEX, UPLOAD_TARGET} from "../main";
 import {Response} from "express";
+
 
 @Injectable()
 export class FileService {
@@ -21,14 +22,17 @@ export class FileService {
 
         files.forEach(file => {
             this.regexArray.forEach(reg => {
-                if (reg.regex.test(file.originalname)) {
+                if (this.regexFromString(reg.regex).test(file.originalname)) {
                     if (reg.status) {
+
+                        Logger.error(`Doubled file type.
+                        regex: ${reg.regex}
+                        filename: ${file.originalname}`)
+
                         let resJSON = {
                             status: "failed",
                             message: "Powtórzony typ pliku",
-
                         }
-
                         return res.status(406).send(resJSON)
                     }
                     reg.status = true
@@ -43,6 +47,8 @@ export class FileService {
                 status: "failed",
                 message: "Brak sensownych plików",
             }
+
+            Logger.warn(`No correct files in request`)
 
             return res.status(406).send(resJSON)
         }
@@ -68,6 +74,11 @@ export class FileService {
             `${UPLOAD_TARGET}/${file.originalname}`,
             new Uint8Array(fileData),
             'binary');
+        Logger.log(`File ${file.originalname} is saved.`)
     }
 
+    private regexFromString(regex: string) {
+        const match = /^\/(.*)\/([a-z]*)$/.exec(regex)
+        return new RegExp(match[1], match[2])
+    }
 }
--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,9 @@ export let REGEX = []
 export async function bootstrap(target?: string, regex?, port?: number ) {
     if(target) UPLOAD_TARGET = target;
     if(regex) REGEX = regex;
-    const app = await NestFactory.create(AppModule, {logger: ['error', 'warn', 'debug', 'verbose', 'log']});
+    const app = await NestFactory.create(AppModule,  {
+        logger: false,
+    });
     app.use('/uploaded', (req, res) => handler(req, res, {public: UPLOAD_TARGET}))
     await app.listen(port || process.env.UPLOAD_PORT || 8080);
 }
